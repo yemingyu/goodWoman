fd236ea214d0ccff80d9dac9529750201b40e2ca
夜禹 <mingyu.ymy@alipay.com>
2018-04-26 21:09:16 +0800

    初步首页和搜索切换

--- page/home.js
+++ page/home.js
@@ -11,6 +11,7 @@ Page({
     typeID: 0,
     isLoading: true,
     loadOver: false,
+    hiddenSearch: true,
     districtList: [{ key: 1, value: "化妆品" }, {
       key: 2, value: "食品"
     }, {
@@ -63,13 +64,13 @@ Page({
 
     // 2 搜索栏初始化
     var that = this;
-    // WxSearch.init(
-    //   that,  // 本页面一个引用
-    //   ['杭州', '嘉兴', "海宁", "桐乡", '宁波', '金华'], // 热点搜索推荐，[]表示不使用
-    //   ['湖北', '湖南', '北京', "南京"],// 搜索匹配，[]表示不使用
-    //   that.mySearchFunction, // 提供一个搜索回调函数
-    //   that.myGobackFunction //提供一个返回回调函数
-    // );
+    WxSearch.init(
+      that,  // 本页面一个引用
+      ['杭州', '嘉兴', "海宁", "桐乡", '宁波', '金华'], // 热点搜索推荐，[]表示不使用
+      ['湖北', '湖南', '北京', "南京"],// 搜索匹配，[]表示不使用
+      that.mySearchFunction, // 提供一个搜索回调函数
+      that.myGobackFunction //提供一个返回回调函数
+    );
 
   },
 
@@ -122,12 +123,31 @@ Page({
   
   },
 
+  gwsearchOpenNavigate: function (e) {
+    // console.log(e);
+    // wx.navigateTo({
+    //   url: 'search/gwsearch',
+    // })
+    var that = this;
+    that.setData({
+      hiddenSearch: false
+    })
+  },
+
 
   // 3 转发函数，固定部分，直接拷贝即可
   wxSearchInput: WxSearch.wxSearchInput,  // 输入变化时的操作
   wxSearchKeyTap: WxSearch.wxSearchKeyTap,  // 点击提示或者关键字、历史记录时的操作
   wxSearchDeleteAll: WxSearch.wxSearchDeleteAll, // 删除所有的历史记录
-  wxSearchConfirm: WxSearch.wxSearchConfirm,  // 搜索函数
+  // wxSearchConfirm: WxSearch.wxSearchConfirm,  // 搜索函数
+
+  wxSearchConfirm: function (e) {
+    var that = this;
+    that.setData({
+      hiddenSearch: true
+    })
+  },
+
   wxSearchClear: WxSearch.wxSearchClear,  // 清空函数
 
   // 4 搜索回调函数  
