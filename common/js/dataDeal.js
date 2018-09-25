79d007d98f1ef1f1ab8e117bb681dea066f0d61a
夜禹 <mingyu.ymy@alipay.com>
2018-09-25 15:27:50 +0800

    数据用dic保存，便于查找 id改用自增 时间问题解决

--- common/js/dataDeal.js
+++ common/js/dataDeal.js
@@ -1,95 +1,100 @@
-// mock数据区
-const firstPage = [{
-  id: "1",
-  category: "水果1",
-  title: "苹果1",
-  thingimage: "http://i3.hoopchina.com.cn/blogfile/201206/29/134095667484768.jpg",
-  useProgress: 83,
-  progressColor: "red",
-  expiredDate: "七天后1"
-},
-{
-  id: "2",
-  category: "水果2",
-  title: "苹果2",
-  progressColor: "yellow",
-  thingimage: "http://i3.hoopchina.com.cn/blogfile/201206/29/134095667484768.jpg",
-  useProgress: 50,
-  expiredDate: "七天后2"
-},
-{
-  id: "3",
-  category: "水果3",
-  title: "苹果3",
-  thingimage: "http://i3.hoopchina.com.cn/blogfile/201206/29/134095667484768.jpg",
-  useProgress: 33,
-  expiredDate: "七天后3"
-},
-{
-  id: "4",
-  category: "水果4",
-  title: "苹果4",
-  thingimage: "http://i3.hoopchina.com.cn/blogfile/201206/29/134095667484768.jpg",
-  useProgress: 45,
-  expiredDate: "七天后4"
-},
-{
-  id: "5",
-  category: "水果5",
-  title: "苹果5",
-  thingimage: "http://i3.hoopchina.com.cn/blogfile/201206/29/134095667484768.jpg",
-  useProgress: 45,
-  expiredDate: "七天后5"
-},
-{
-  id: "6",
-  category: "水果6",
-  title: "苹果6",
-  thingimage: "http://i3.hoopchina.com.cn/blogfile/201206/29/134095667484768.jpg",
-  useProgress: 83,
-  expiredDate: "七天后6"
-}]
+// // mock数据区
+// const firstPage = [{
+//   id: "1",
+//   category: "水果1",
+//   title: "苹果1",
+//   thingimage: "http://i3.hoopchina.com.cn/blogfile/201206/29/134095667484768.jpg",
+//   useProgress: 83,
+//   progressColor: "red",
+//   expiredDate: "七天后1"
+// },
+// {
+//   id: "2",
+//   category: "水果2",
+//   title: "苹果2",
+//   progressColor: "yellow",
+//   thingimage: "http://i3.hoopchina.com.cn/blogfile/201206/29/134095667484768.jpg",
+//   useProgress: 50,
+//   expiredDate: "七天后2"
+// },
+// {
+//   id: "3",
+//   category: "水果3",
+//   title: "苹果3",
+//   thingimage: "http://i3.hoopchina.com.cn/blogfile/201206/29/134095667484768.jpg",
+//   useProgress: 33,
+//   expiredDate: "七天后3"
+// },
+// {
+//   id: "4",
+//   category: "水果4",
+//   title: "苹果4",
+//   thingimage: "http://i3.hoopchina.com.cn/blogfile/201206/29/134095667484768.jpg",
+//   useProgress: 45,
+//   expiredDate: "七天后4"
+// },
+// {
+//   id: "5",
+//   category: "水果5",
+//   title: "苹果5",
+//   thingimage: "http://i3.hoopchina.com.cn/blogfile/201206/29/134095667484768.jpg",
+//   useProgress: 45,
+//   expiredDate: "七天后5"
+// },
+// {
+//   id: "6",
+//   category: "水果6",
+//   title: "苹果6",
+//   thingimage: "http://i3.hoopchina.com.cn/blogfile/201206/29/134095667484768.jpg",
+//   useProgress: 83,
+//   expiredDate: "七天后6"
+// }]
 
-const districtListMock = [{ key: 1, value: "化妆品" }, {
-  key: 2, value: "食品"
-}, {
-  key: 3, value: "价格最高"
-}, {
-  key: 4, value: "服务最好"
-}, {
-  key: 5, value: "环境最好"
-}, {
-  key: 6, value: "预约最快"
-}]
-const sortingListMock = [{ key: 1, value: "智能排序" }, {
-  key: 2, value: "价格最低"
-}, {
-  key: 3, value: "价格最高"
-}, {
-  key: 4, value: "服务最好"
-}, {
-  key: 5, value: "环境最好"
-}, {
-  key: 6, value: "预约最快"
-}]
+// const districtListMock = [{ key: 1, value: "化妆品" }, {
+//   key: 2, value: "食品"
+// }, {
+//   key: 3, value: "价格最高"
+// }, {
+//   key: 4, value: "服务最好"
+// }, {
+//   key: 5, value: "环境最好"
+// }, {
+//   key: 6, value: "预约最快"
+// }]
+// const sortingListMock = [{ key: 1, value: "智能排序" }, {
+//   key: 2, value: "价格最低"
+// }, {
+//   key: 3, value: "价格最高"
+// }, {
+//   key: 4, value: "服务最好"
+// }, {
+//   key: 5, value: "环境最好"
+// }, {
+//   key: 6, value: "预约最快"
+// }]
 
-const filterListMock = [{ key: 1, value: "周日营业", selected: false }, {
-  key: 2, value: "官方假期营业（香港）", selected: false
-}, {
-  key: 3, value: "可为儿童接种疫苗", selected: false
-}, {
-  key: 4, value: "网上付款", selected: false
-}, {
-  key: 5, value: "到诊所现场付款", selected: false
-}]
+// const filterListMock = [{ key: 1, value: "周日营业", selected: false }, {
+//   key: 2, value: "官方假期营业（香港）", selected: false
+// }, {
+//   key: 3, value: "可为儿童接种疫苗", selected: false
+// }, {
+//   key: 4, value: "网上付款", selected: false
+// }, {
+//   key: 5, value: "到诊所现场付款", selected: false
+// }]
 
 
 
 var AllDataValue = [];
 var AllDealedDataValue = [];
-var AllDataKey = "AllThingsKey";
+var AllDataKey = "AllThingsKeyTest1";
 var AllDataPageSize = 5;
 var AllCurrentData = [];
+var AllDataCurrentIdKey = "AllDataCurrentIdKeyTest2"
+
+var AllDataDicValue = {};
+var AllDataDicKey = "AllThingsDicKeyTest";
+// var AllDataDicValueIdKey = "AllDataDicValueIdKey"
 
 var AllCategory = ['水果', '化妆品', '饮料'];
 
@@ -114,16 +119,16 @@ function transformThingData(data) {
     var currentDate = new Date();
 
   var allDays = Math.floor((ValidityDate.getTime() - OpenDate.getTime()) / (24 * 3600 * 1000)) 
-  var remainedDays = ValidityDate.getTime() - currentDate.getTime()
-  var remainedDaysPercent = Math.floor((ValidityDate.getTime() - currentDate.getTime()) / allDays * 100)
+  var remainedDays = Math.floor((ValidityDate.getTime() - currentDate.getTime()) / (24 * 3600 * 1000))
+  var costDaysPercent = Math.floor((allDays - remainedDays) / allDays * 100)
 
-  resultData["useProgress"] = remainedDaysPercent
-  if (remainedDaysPercent > 30) {
-    resultData["progressColor"] = "green"
-  } else if (remainedDaysPercent > 10) {
+  resultData["useProgress"] = costDaysPercent
+  if (costDaysPercent > 90) {
+    resultData["progressColor"] = "red"
+  } else if (costDaysPercent > 70) {
     resultData["progressColor"] = "yellow"
   } else {
-    resultData["progressColor"] = "red"
+    resultData["progressColor"] = "green"
   }
   if (remainedDays <= 0) {
     resultData["expiredDate"] = "已过期"
@@ -134,7 +139,16 @@ function transformThingData(data) {
 }
 
 function getAllDataStorage() {
-  AllDataValue = wx.getStorageSync(AllDataKey)
+  AllDataDicValue = wx.getStorageSync(AllDataDicKey)
+  if (AllDataDicValue == null || AllDataDicValue == "") {
+    AllDataDicValue = {}
+  }
+  var i = 0
+  for (var key in AllDataDicValue) {
+    AllDataValue[i++] = AllDataDicValue[key]
+  }
+  // AllDataValue = wx.getStorageSync(AllDataKey)
+
   var AllDealedDataValue = []
   // 对数据进行处理
   for (var i = 0; i < AllDataValue.length; i++) {
@@ -181,7 +195,7 @@ function getAllDataStorageWithxxx() {
 function addAllDataStorageWithData(data, completion) {
   setTimeout(() => {
     var resultData = {}
-    resultData["id"] = (AllDataValue.length + 1).toString
+    resultData["id"] = getCurrentId()
     resultData["category"] = data["category"]
     resultData["title"] = data["title"]
     // 图片需要上传后获取链接才能使用
@@ -192,8 +206,10 @@ function addAllDataStorageWithData(data, completion) {
     resultData["OpenDate"] = data["OpenDate"]
     resultData["AlarmDate"] = data["AlarmDate"]
 
-    AllDataValue[AllDataValue.length] = resultData
-    wx.setStorageSync(AllDataKey, AllDataValue)
+    // AllDataValue[AllDataValue.length] = resultData
+    // wx.setStorageSync(AllDataKey, AllDataValue)
+    AllDataDicValue[resultData["id"]] = resultData
+    wx.setStorageSync(AllDataDicKey, AllDataDicValue)
     completion(true)
   }, 3000)
 }
@@ -207,11 +223,19 @@ function updateAllDataStorageWithxxx() {
 }
 
 function getCurrentId() {
-  return wx.getStorageSync("AllDataCurrentIdKey")
+  var currentId = wx.getStorageSync(AllDataCurrentIdKey)
+  var currentIdNum
+  if (currentId == null || currentId == "") {
+    currentId = 1   // 从1开始
+  }
+  currentIdNum = new Number(currentId);
+  var currentIdStr = currentId.toString()
+  setCurrentId(currentId + 1)
+  return currentIdStr
 }
 
 function setCurrentId(currentId) {
-  wx.setStorageSync("AllDataCurrentIdKey", currentId)
+  wx.setStorageSync(AllDataCurrentIdKey, currentId)
 }
 
 // 导出接口
